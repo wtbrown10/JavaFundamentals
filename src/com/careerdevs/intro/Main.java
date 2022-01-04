5ed0e10d6a8455d19c30d4a0ae94bdc155d4a0d6
Will brown <wtbrown10@gmail.com>
2022-01-03 21:38:18 -0500

    challenge 2/3

--- src/com/careerdevs/intro/Main.java
+++ src/com/careerdevs/intro/Main.java
@@ -2,15 +2,19 @@ package com.careerdevs.intro;
 
 public class Main {
 
-
+    static void createFullName(String first, String last){
+        String value = first + " " + last;
+        System.out.println(value);
+    }
 
     public static void main(String[] args) {
-        String firstName = "Will";
-        String lastName = "Brown";
+//        String firstName = "Will";
+//        String lastName = "Brown";
+//
+//        String fullName = firstName + " " + lastName;
+//        System.out.println(fullName);
 
-        String fullName = firstName + " " + lastName;
-        System.out.println(fullName);
-
+        createFullName("Gabriel", "Sherman");
     }
 
 
