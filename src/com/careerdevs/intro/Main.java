0ac57d6bc9f1dff0846f118f42c50f6bc5e12434
Will brown <wtbrown10@gmail.com>
2022-01-03 21:42:53 -0500

    challenge 3/3

--- src/com/careerdevs/intro/Main.java
+++ src/com/careerdevs/intro/Main.java
@@ -15,6 +15,7 @@ public class Main {
 //        System.out.println(fullName);
 
         createFullName("Gabriel", "Sherman");
+        NameGenerator.generateFullName("Sam", "Thomson");
     }
 
 
---
+++ src/com/careerdevs/intro/NameGenerator.java
@@ -0,0 +1,8 @@
+package com.careerdevs.intro;
+
+public class NameGenerator {
+
+    static void generateFullName(String first, String last){
+        System.out.println(first + " " + last);
+    }
+}
