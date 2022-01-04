e82e3f5e2d8a9179649d97e5f52d66eddba1a1f5
Will brown <wtbrown10@gmail.com>
2022-01-03 21:36:47 -0500

    challenge 1/3

---
+++ src/com/careerdevs/intro/Main.java
@@ -0,0 +1,25 @@
+package com.careerdevs.intro;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        String firstName = "Will";
+        String lastName = "Brown";
+
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+    }
+
+
+
+
+
+
+
+}
+
+
+
