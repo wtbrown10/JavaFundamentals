e546c677a774e5f6dbe8ce27f5c8bfff4aa16900
Will brown <wtbrown10@gmail.com>
2022-12-02 13:13:01 -0500

    search algo & sorted algo

---
+++ src/com/careerdevs/will/SearchInsertPosition.java
@@ -0,0 +1,18 @@
+package com.careerdevs.will;
+
+public class SearchInsertPosition {
+}
+
+//class Solution {
+//
+//    public int searchInsert(int[] nums, int target) {
+//        int value = Arrays.binarySearch(nums, target);
+//        if(value >= 0){
+//            return value;
+//        } else value = (value*-1) -1; //binarySearch when number not found returns
+//        //the position as a negative number where the item should be not the index
+//        //so i converted it to a positive int and subtracted 1
+//        return value;
+//    }
+//
+//}
---
+++ src/com/careerdevs/will/SquaresOfASortedArray.java
@@ -0,0 +1,20 @@
+package com.careerdevs.will;
+
+public class SquaresOfASortedArray {
+}
+
+//class Solution {
+//    // public int[] arrayMaker()
+//
+//    public int[] sortedSquares(int[] nums) {
+//        int[] nums2;
+//        nums2 = new int[nums.length];
+//        int a = 0;
+//        for (int i = 0; i < nums.length; i++ ){
+//            a = nums[i];
+//            nums2[i] = (int)Math.pow(a, 2);
+//        }
+//        Arrays.sort(nums2);
+//        return nums2;
+//    }
+//}
